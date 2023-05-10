4e0c36bc4fdd9efa79ca66b1ca205c8c2e3eb913
William George <42682665+wgeorge1990@users.noreply.github.com>
2023-05-10 09:40:42 -0400

    REMOVE: axios and convert requests to use fetch with headers: application/json, did not resolve issue...

--- src/react-app/src/components/saved-urls-manager/SavedUrlsManager.jsx
+++ src/react-app/src/components/saved-urls-manager/SavedUrlsManager.jsx
@@ -1,5 +1,5 @@
 import React,{ useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const SavedUrlsManager = () => {
     const [urls, setUrls] = useState([]);
@@ -12,37 +12,38 @@ const SavedUrlsManager = () => {
     };
 
     useEffect(() => {
-        // Add an event listener to the select element
-        const select = document.getElementById('savedUrls');
-        if (select) {
-            select.addEventListener('change', (e) => updateJspInputField(e.target.value));
+      // Add an event listener to the select element
+      const select = document.getElementById('savedUrls');
+      if (select) {
+        select.addEventListener('change', (e) => updateJspInputField(e.target.value));
+      }
+
+      fetch('/ProjectPropertiesGetUrls', {
+        headers: {
+          'Content-Type': 'application/json'
         }
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          setUrls(data);
+          updateSelectOptions(data);
+        })
+        .catch(error => {
+          console.error('Error fetching saved URLs:', error);
+        });
 
-         axios.get('/ProjectPropertiesGetUrls')
-            .then(response => {
-                // requests are currently breaking react so left commented for time being
-                // setUrls(response.data);
-                // updateSelectOptions(response.data);
-                console.log('response:', response)
-                console.log('response.data:', response.data)
-
-            })
-            .catch(error => {
-                console.error('Error fetching saved URLs:', error);
-            });
-
-
-        // Remove the event listener when the component unmounts
-        return () => {
-            if (select) {
-                select.removeEventListener('change', (e) => updateJspInputField(e.target.value));
-            }
-        };
+      // Remove the event listener when the component unmounts
+      return () => {
+        if (select) {
+          select.removeEventListener('change', (e) => updateJspInputField(e.target.value));
+        }
+      };
     }, []);
-
-    useEffect(() => {
-
-    },[])
 
 
 
@@ -57,44 +58,112 @@ const SavedUrlsManager = () => {
             select.appendChild(option);
         });
     };
+//
+//     const addUrl = () => {
+//         axios.post('/ProjectPropertiesAddUrl', { url: currentUrl })
+//             .then(response => {
+//                 setUrls(prevUrls => [...prevUrls, currentUrl]);
+//                 resetInput();
+//                 updateSelectOptions([...urls, currentUrl]);
+//                 updateJspInputField(currentUrl); // update the JSP input field
+//             })
+//             .catch(error => {
+//                 console.error('Error adding URL:', error);
+//             });
+//     };
 
     const addUrl = () => {
-        axios.post('/ProjectPropertiesAddUrl', { url: currentUrl })
-            .then(response => {
-                setUrls(prevUrls => [...prevUrls, currentUrl]);
-                resetInput();
-                updateSelectOptions([...urls, currentUrl]);
-                updateJspInputField(currentUrl); // update the JSP input field
-            })
-            .catch(error => {
-                console.error('Error adding URL:', error);
-            });
+      fetch('/ProjectPropertiesAddUrl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: currentUrl })
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        setUrls(prevUrls => [...prevUrls, currentUrl]);
+        resetInput();
+        updateSelectOptions([...urls, currentUrl]);
+        updateJspInputField(currentUrl); // update the JSP input field
+      })
+      .catch(error => {
+        console.error('Error adding URL:', error);
+      });
     };
 
+
+//     const updateUrl = (oldUrl, newUrl) => {
+//         axios.post('/ProjectPropertiesUpdateUrl', { oldUrl, newUrl })
+//             .then(response => {
+//                 setUrls(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
+//                 updateSelectOptions(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
+//                 updateJspInputField(newUrl); // update the JSP input field
+//             })
+//             .catch(error => {
+//                 console.error('Error updating URL:', error);
+//             });
+//     }
+
     const updateUrl = (oldUrl, newUrl) => {
-        axios.post('/ProjectPropertiesUpdateUrl', { oldUrl, newUrl })
-            .then(response => {
-                setUrls(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
-                updateSelectOptions(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
-                updateJspInputField(newUrl); // update the JSP input field
-            })
-            .catch(error => {
-                console.error('Error updating URL:', error);
-            });
-    }
+      fetch('/ProjectPropertiesUpdateUrl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ oldUrl, newUrl })
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          setUrls(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
+          updateSelectOptions(prevUrls => prevUrls.map(url => url === oldUrl ? newUrl : url));
+          updateJspInputField(newUrl); // update the JSP input field
+        })
+        .catch(error => {
+          console.error('Error updating URL:', error);
+        });
+    };
+
+//     const deleteUrl = index => {
+//         const urlToDelete = urls[index];
+//         axios.post('/ProjectPropertiesDeleteUrl', { url: urlToDelete })
+//             .then(response => {
+//                 let newUrls = urls.filter((_, i) => i !== index);
+//                 setUrls(newUrls);
+//                 updateSelectOptions(newUrls);
+//                 updateJspInputField(newUrls[0]); // update the JSP input field
+//             })
+//             .catch(error => {
+//                 console.error('Error deleting URL:', error);
+//             });
+//     };
+
 
     const deleteUrl = index => {
-        const urlToDelete = urls[index];
-        axios.post('/ProjectPropertiesDeleteUrl', { url: urlToDelete })
-            .then(response => {
-                let newUrls = urls.filter((_, i) => i !== index);
-                setUrls(newUrls);
-                updateSelectOptions(newUrls);
-                updateJspInputField(newUrls[0]); // update the JSP input field
-            })
-            .catch(error => {
-                console.error('Error deleting URL:', error);
-            });
+      const urlToDelete = urls[index];
+      fetch('/ProjectPropertiesDeleteUrl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: urlToDelete })
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        let newUrls = urls.filter((_, i) => i !== index);
+        setUrls(newUrls);
+        updateSelectOptions(newUrls);
+        updateJspInputField(newUrls[0]); // update the JSP input field
+      })
+      .catch(error => {
+        console.error('Error deleting URL:', error);
+      });
     };
 
     const saveUrl = () => {
